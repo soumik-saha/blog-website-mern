32691df237db7aa06520d508389a4341326083d4
Soumik Saha <sahasoumik1573@gmail.com>
2023-04-24 15:51:35 +0530

    Updated

--- api/index.js
+++ api/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
-
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const User = require("./models/user");
 const app = express();
 
-app.get('/test', (req, res) => {
-    res.send("testing ok2");
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://blog:m5cMeQcnBGxR88NM@cluster0.cvrmc86.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.create({ username, password });
+    res.json(userDoc);
 });
 
 app.listen(4000);
+
+// blog (username)
+// m5cMeQcnBGxR88NM (password)
+
+// mongodb+srv://blog:m5cMeQcnBGxR88NM@cluster0.cvrmc86.mongodb.net/?retryWrites=true&w=majority  
+// (connection string)
