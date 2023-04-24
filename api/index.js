102356610d57065fe7b84916cc43d863ee16bdae
Soumik Saha <sahasoumik1573@gmail.com>
2023-04-24 12:45:08 +0530

    adding dependencies

--- api/index.js
+++ api/index.js
@@ -1,1 +1,9 @@
-const express = require('express');
+const express = require("express");
+
+const app = express();
+
+app.get('/test', (req, res) => {
+    res.send("testing ok2");
+});
+
+app.listen(4000);
